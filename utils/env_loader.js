ff472fb36acd815a16571d37f55ae3292e04a442
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-12-02 19:38:04 +0200

    add DBClient class + envLoader

---
+++ utils/env_loader.js
@@ -0,0 +1,15 @@
+import { existsSync, readFileSync } from "fs";
+
+const envLoader = () => {
+	const env = process.env.npm_lifecycle_event || "dev";
+	const path = env.includes("test") ? ".env.test" : ".env";
+	if (existsSync(path)) {
+		const data = readFileSync(path, { encoding: "utf8" }).trim().split("\n");
+		for (const line of data) {
+			const envVar = line.indexOf("=");
+			process.env[line.substring(0, envVar)] = line.substring(envVar + 1);
+		}
+	}
+};
+
+export default envLoader;
